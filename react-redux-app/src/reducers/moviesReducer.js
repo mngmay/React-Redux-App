3a66c9b7fb3623b7b79a0c967dcf4322c93637d8
May Ng <mngmay@gmail.com>
2019-08-15 12:25:57 -0700

    created directories to organize actions, components, reducers. Created Redux store.

---
+++ react-redux-app/src/reducers/moviesReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  movies: [],
+  isLoading: false,
+  error: ""
+};
+
+export const moviesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
