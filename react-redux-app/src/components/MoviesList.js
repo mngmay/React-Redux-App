11cf84dbdc9197db01ac163d3b7047e68738d069
May Ng <mngmay@gmail.com>
2019-08-15 22:31:56 -0700

    removed unnecessary classes from index.css

--- react-redux-app/src/components/MoviesList.js
+++ react-redux-app/src/components/MoviesList.js
@@ -22,7 +22,7 @@ const MoviesList = props => {
       {props.isLoading && (
         <Loader type="tailspin" color="#00BFFF" height="15" width="100" />
       )}
-      <div className="movies-list all">
+      <div className="movies-list">
         {props[props.list].map(movie => (
           <Movie key={movie.id} movie={movie} addMovie={addMovie} />
         ))}
