a7ded8bd41ffd189960fee35d3db5fd45b12d397
May Ng <mngmay@gmail.com>
2019-08-18 12:01:23 -0700

    Created search component

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MoviesNav from "./components/MoviesNav";
 import MoviesList from "./components/MoviesList";
 import Header from "./components/Header";
+import { Search } from "./components/Search";
 
 function App() {
   const [list, setList] = useState("movies");
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <MoviesNav setList={setList} />
+      <Search />
       <MoviesList list={list} />
     </div>
   );
---
+++ react-redux-app/src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Search = () => {
+  return (
+    <>
+      <input type="text" name="search" placeholder="Search Movies" />
+    </>
+  );
+};
